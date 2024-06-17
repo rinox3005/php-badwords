e8fd2ffa82f3fa0acb6779018fbf8023d039ecda
rinox3005 <peppe.rinox@gmail.com>
2024-06-17 17:27:43 +0200

    [UPD] created new variabile to calculate censored text and new length

--- badwords.php
+++ badwords.php
@@ -2,6 +2,8 @@
 $word = $_GET['word'];
 $text = $_GET['text'];
 
+$censoredText = str_replace($word, '***', $text);
+
 // // concatena la due elementi con uno spazio nel mezzo
 // echo $word . ' ' . $text;
 
@@ -43,7 +45,7 @@ $text = $_GET['text'];
         <h1 class="text-center pt-5 pb-3">Bad Word?</h1>
         <section class="container mt-4 m-auto row">
             <div class="col-6 border p-4">
-                <h2 class="text-center mb-5">Text not censored</h2>
+                <h2 class="text-center mb-5">Uncensored Text</h2>
                 <h5 class="d-inline">Word to censor:</h5>
                 <span class="fs-6 ">"<?php echo $word ?>"</span>
                 <h5 class="mt-5">Text to check:</h5>
@@ -51,7 +53,15 @@ $text = $_GET['text'];
                 <h5 class="d-inline">Text length:</h5>
                 <span class="fs-6">"<?php echo strlen($text) ?>"</span>
             </div>
-
+            <div class="col-6 border p-4">
+                <h2 class="text-center mb-5">Censored Text</h2>
+                <h5 class="d-inline">Word to censor:</h5>
+                <span class="fs-6 ">"<?php echo $word ?>"</span>
+                <h5 class="mt-5">Text to check (censored):</h5>
+                <p class="fs-6 mb-5">"<?php echo $censoredText; ?>"</p>
+                <h5 class="d-inline">Text length:</h5>
+                <span class="fs-6">"<?php echo strlen($censoredText) ?>"</span>
+            </div>
         </section>
     </main>
 </body>
